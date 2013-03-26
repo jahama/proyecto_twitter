2a04f88b31b1e22f56d6352a976425ce0b4639b0
iMac 9 <imac9@iMac-de-iMac-4.local>
2013-03-26 18:44:52 +0100

    Creado el listener para elcambio de una pestana a otra. Tambien he creado los fragmentos para cada pestana, pero no se muestra nada en ellos

--- gen/com/jahama/proyectotwitter/R.java
+++ gen/com/jahama/proyectotwitter/R.java
@@ -25,14 +25,15 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
-        public static final int buttonLogo=0x7f080001;
-        public static final int fragmentContainer=0x7f080002;
-        public static final int fragmentText=0x7f080000;
+        public static final int buttonLogo=0x7f080002;
+        public static final int fragmentContainer=0x7f080000;
+        public static final int fragmentText=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment=0x7f030001;
-        public static final int main=0x7f030002;
+        public static final int evento_fragment=0x7f030001;
+        public static final int fragment=0x7f030002;
+        public static final int main=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/jahama/proyectotwitter/MainActivity.java
+++ src/com/jahama/proyectotwitter/MainActivity.java
@@ -1,60 +1,49 @@
 package com.jahama.proyectotwitter;
 
+
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.util.Log;
 import android.view.Menu;
 
 
 
 public class MainActivity extends Activity {
 
+	protected static final String TAG = "MAIN ACTIVITY";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		/*
-		 * Action Bar
-		 */
+		// Action Bar. Configuración para pestañas de la barra de acción		
 		ActionBar actionBar = getActionBar();
 		actionBar.setTitle("CapaTwitter");
 		actionBar.setSubtitle("Proyecto: Android");
 		// Navegacion con pestanas
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		
-		// Create a tab listener that is called when the user changes tabs.
-		/*
-		 * Listener que seran llamados cuando se el usuario cambie de pestañas
-		 */
-	    ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-	        public void onTabSelected(ActionBar.Tab tab,
-	                FragmentTransaction ft) { }
-
-	        public void onTabUnselected(ActionBar.Tab tab,
-	                FragmentTransaction ft) { }
-
-	        public void onTabReselected(ActionBar.Tab tab,
-	                FragmentTransaction ft) { }
-	    };
-		
+				
 		/*
 		 * Pestañas de la barra de accion
 		 */
 		Tab tabTweets = actionBar.newTab();
 		tabTweets.setText("Tweets")
 					.setContentDescription("Tweets") 
-					.setTabListener(tabListener);
+					.setTabListener(new MyTabListener<MyFragment>(this, "tweets", MyFragment.class));
 		actionBar.addTab(tabTweets);
 
 		Tab tabEventos = actionBar.newTab();
 		tabEventos.setText("Eventos")
 					.setContentDescription("Eventos") 
-					.setTabListener(tabListener);
+						.setTabListener(new MyTabListener<EventosFragment>(this, "eventos",	EventosFragment.class));
 		actionBar.addTab(tabEventos);
 
 	}
@@ -72,48 +61,57 @@ public class MainActivity extends Activity {
 	  */
 	 
 	
-	public static class TabListener<T extends Fragment> 
-	    implements ActionBar.TabListener {
-	    
-	    private MyFragment fragment;
-	    private Activity activity;
-	    private Class<T> fragmentClass;
-	    private int fragmentContainer;
+public static class MyTabListener<T extends Fragment> implements  TabListener {
+	private Fragment mFragment;
+	private final Activity mActivity;
+	private final String mTag;
+	private final Class<T> mClass;
 
-	    public TabListener(Activity activity, int fragmentContainer, 
-	                       Class<T> fragmentClass) {
 
-	      this.activity = activity;
-	      this.fragmentContainer = fragmentContainer;
-	      this.fragmentClass = fragmentClass;
-	    }
+	/**
+	 * Constructor used each time a new tab is created.
+	 * 
+	 * @param activity
+	 *            The host Activity, used to instantiate the fragment
+	 * @param tag
+	 *            The identifier tag for the fragment
+	 * @param clz
+	 *            The fragment's Class, used to instantiate the fragment
+	 */
 
-	    // Called when a new tab has been selected
-	   
-		public void onTabSelected(Tab tab, FragmentTransaction ft) {
-	      if (fragment == null) {
-	        String fragmentName = fragmentClass.getName();
-	        fragment =
-	          (MyFragment)Fragment.instantiate(activity, fragmentName);
-	        ft.add(fragmentContainer, fragment, null);
-	        fragment.setFragmentText(tab.getText());
-	      } else {
-	        ft.attach(fragment);
-	      }
-	    }
+	public MyTabListener(Activity activity, String tag, Class<T> clz) {
+		mActivity = activity;
+		mTag = tag;
+		mClass = clz;
+	}
 
-	    // Called on the currently selected tab when a different tag is
-	    // selected. 
-	    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-	      if (fragment != null) {
-	        ft.detach(fragment);
-	      }
-	    } 
+	/* The following are each of the ActionBar.TabListener callbacks */
 
-	    // Called when the selected tab is selected.
-	    public void onTabReselected(Tab tab, FragmentTransaction ft) {
-	      // TODO React to a selected tab being selected again.
-	    }
-	  }
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		Log.i(TAG," -- onTabSelected -- ");
+		// Check if the fragment is already initialized
+		if (mFragment == null) {
+			// If not, instantiate and add it to the activity
+			mFragment = Fragment.instantiate(mActivity, mClass.getName());
+			ft.add(android.R.id.content, mFragment, mTag);
+		} else {
+			// If it exists, simply attach it in order to show it
+			//ft.setCustomAnimations(android.R.animator.fade_in,R.animator.animationtest);
+			Log.d(TAG," -- onTabSelected -- " + mFragment.getTag());
+			ft.attach(mFragment);
+		}
+	}
 
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		Log.i(TAG," -- onTabUnselected -- ");
+		if (mFragment != null) {
+			//ft.setCustomAnimations(android.R.animator.fade_in, R.animator.test);
+			ft.detach(mFragment);
+		}
+	}
+
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		Log.i(TAG," -- onTabReselected -- ");
+	}
+}
 }
