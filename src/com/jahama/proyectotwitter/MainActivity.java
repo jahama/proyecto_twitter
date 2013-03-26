f1f809a1d83d83e4854cace554463314b829b1d5
Javi de la Haza <javi.delahaza@gmail.com>
2013-03-26 13:27:27 +0100

    Empezando con la barra de accion y añadiedo las pestañas para la vista de Tweets y Eventos

--- src/com/jahama/proyectotwitter/MainActivity.java
+++ src/com/jahama/proyectotwitter/MainActivity.java
@@ -1,15 +1,43 @@
 package com.jahama.proyectotwitter;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.view.Menu;
 
+
+@SuppressLint("NewApi")
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		/*
+		 * Action Bar
+		 */
+		ActionBar actionBar = getActionBar();
+		actionBar.setTitle("CapaTwitter");
+		actionBar.setSubtitle("Proyecto: Android");
+		// Navegacion con pestñas
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		// Creo la pestaña de tweets
+		
+		Tab pestanaTweets = actionBar.newTab();
+		pestanaTweets.setText("Tweets")
+					.setIcon(R.drawable.ic_launcher)
+					.setContentDescription("Tweets") 
+					.setTabListener(
+					 new TabListener<MyFragment>
+					(this, R.id.fragmentContainer, MyFragment.class));
+		actionBar.addTab(pestanaTweets);
+
+
 	}
 
 	@Override
@@ -18,5 +46,53 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu); 
 		return true;
 	}
+	
+	/**
+	  *   clase para controlar la pestañas del la barra de accion
+	  */
+	  @SuppressLint("NewApi")
+	public static class TabListener<T extends Fragment> 
+	    implements ActionBar.TabListener {
+	    
+	    private MyFragment fragment;
+	    private Activity activity;
+	    private Class<T> fragmentClass;
+	    private int fragmentContainer;
+
+	    public TabListener(Activity activity, int fragmentContainer, 
+	                       Class<T> fragmentClass) {
+
+	      this.activity = activity;
+	      this.fragmentContainer = fragmentContainer;
+	      this.fragmentClass = fragmentClass;
+	    }
+
+	    // Called when a new tab has been selected
+	    @SuppressLint("NewApi")
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+	      if (fragment == null) {
+	        String fragmentName = fragmentClass.getName();
+	        fragment =
+	          (MyFragment)Fragment.instantiate(activity, fragmentName);
+	        ft.add(fragmentContainer, fragment, null);
+	        fragment.setFragmentText(tab.getText());
+	      } else {
+	        ft.attach(fragment);
+	      }
+	    }
+
+	    // Called on the currently selected tab when a different tag is
+	    // selected. 
+	    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	      if (fragment != null) {
+	        ft.detach(fragment);
+	      }
+	    } 
+
+	    // Called when the selected tab is selected.
+	    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	      // TODO React to a selected tab being selected again.
+	    }
+	  }
 
 }
